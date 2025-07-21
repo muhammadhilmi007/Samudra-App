2a1ab1d237f3a8c463d5749ab3fc39cc7b549d03
Muhammad Hilmi Arsyad <muhammadhilmiarsyad@gmail.com>
2025-07-21 10:57:51 +0700

    Fix: UsersSchema Modules

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,12 +4,12 @@ const validator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
-      required: [true, "Username is required!"],
+      required: [true, "Name is required!"],
       unique: true,
       trim: true,
-      minlength: [3, "Username must be at least 3 characters long"],
+      minlength: [3, "Name must be at least 3 characters long"],
     },
     email: {
       type: String,
@@ -23,27 +23,6 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Password is required!"],
       minlength: [6, "Password must be at least 6 characters long"],
-    },
-    firstname: {
-      type: String,
-      required: [true, "First name is required!"],
-      trim: true,
-    },
-    lastname: {
-      type: String,
-      required: [true, "Last name is required!"],
-      trim: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: [true, "Phone number is required!"],
-      trim: true,
-      validate: {
-        validator: function (v) {
-          return /^[0-9+\-\s()]+$/.test(v);
-        },
-        message: "Please enter a valid phone number!",
-      },
     },
     branch_id: {
       type: mongoose.Schema.Types.ObjectId,
@@ -80,14 +59,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// Virtual for full name
-userSchema.virtual("fullname").get(function () {
-  return `${this.firstname} ${this.lastname}`;
-});
-
-// Ensure virtual fields are included in JSON
-userSchema.set("toJSON", { virtuals: true });
-
-userSchema.plugin(validator, { message: "{PATH} already exists!" });
+userSchema.plugin(validator, { message: "Email address already exists !" });
 
 module.exports = userSchema;
